2fd94cf0e7e99634ac0026f666b32344b477e151
AnAn1910 <anguongmau@gmail.com>
2023-04-14 00:15:45 +0700

    Add

--- app/src/main/java/com/example/movie/single_movie_details/SingleMovie.kt
+++ app/src/main/java/com/example/movie/single_movie_details/SingleMovie.kt
@@ -2,6 +2,7 @@ package com.example.movie.single_movie_details
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.movie.R
 
 class SingleMovie : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
