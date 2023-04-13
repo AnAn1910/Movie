9dc48820236ac12435e7e49c990aeceb25affe0f
AnAn1910 <anguongmau@gmail.com>
2023-04-13 23:21:01 +0700

    Initial commit

---
+++ app/src/main/java/com/example/movie/single_movie_details/SingleMovie.kt
@@ -0,0 +1,11 @@
+package com.example.movie.single_movie_details
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SingleMovie : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single_movie)
+    }
+}
